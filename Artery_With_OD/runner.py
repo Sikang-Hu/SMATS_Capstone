f579d269b4e9e7ee5d3abd4f2d685caf16577dcc
sikang hu <sikang_hu@berkeley.edu>
2018-04-26 12:42:22 -0700

    modify on runner

--- Artery_With_OD/runner.py
+++ Artery_With_OD/runner.py
@@ -4,6 +4,29 @@ from __future__ import print_function
 import os
 import subprocess
 import sys
+import shutil
+import re
+
+
+def newcfg(old_cfg, new_cfg):
+    """
+    Generate a new configuration file and set the input and output.
+    :param old_cfg:
+    :param new_cfg:
+    :return:
+    """
+    new_cfg = new_cfg + ".sumocfg"
+    old_cfg = old_cfg + ".sumocfg"
+    try:
+        shutil.copy(old_cfg, new_cfg)
+    except:
+        print("Missing", old_cfg)
+
+    file = open(new_cfg, "r")
+    cfg = file.read()
+    file.close()
+
+
 
 
 def runner(file, stat):
@@ -11,6 +34,10 @@ def runner(file, stat):
     file: the name of configuration file
     stat: output statistic result such as RouteLength, WaitingTime, etc.
     """
+    file = file + ".sumocfg"
+    if not os.path.isfile(file):
+        print("Can't find " + file)
+        return
     try:
         sys.path.append(os.path.dirname(
             __file__))  # tutorial in tests
@@ -25,7 +52,7 @@ def runner(file, stat):
     if stat:
         com = "--duration-log.statistics"
     else:
-        com = ""
+        com = "-"
     retcode = subprocess.call(
         [sumoBinary, "-c", file, com], stdout=sys.stdout, stderr=sys.stderr)
     print(">> Simulation closed with status %s" % retcode)
